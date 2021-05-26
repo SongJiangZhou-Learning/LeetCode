33a74e0078c33908c42a5f20fac62ffac499790f
SongJiangzhou <hhsongjz@163.com>
2021-05-27 00:49:07 +0800

    406. 根据身高重建队列

---
+++ 406. 根据身高重建队列/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, ((o1, o2) -> {
+            if (o1[0] != o2[0]) {
+                return o1[0] - o2[0];
+            } else {
+                return o2[1] - o1[1];
+            }
+        }));
+
+        System.out.println(Arrays.deepToString(people));
+        int[][] result = new int[people.length][];
+        for (int[] person : people) {
+            int ready = 0;
+            int notReady = 0;
+            for (int[] seat : result) {
+                if (seat == null) {
+                    notReady++;
+                } else {
+                    ready++;
+                    continue;
+                }
+                if (notReady > person[1]) {
+                    break;
+                }
+            }
+            result[ready + notReady - 1] = person;
+        }
+        return result;
+    }
+}
